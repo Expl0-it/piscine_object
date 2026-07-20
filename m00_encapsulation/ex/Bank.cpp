1d7623b05958d50fb9024195f9f166cbac5d983a
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2026-07-20 20:17:02 +0200

    added constructors to Bank

--- m00_encapsulation/ex/Bank.cpp
+++ m00_encapsulation/ex/Bank.cpp
@@ -1,18 +1,53 @@
 #include "Bank.hpp"
+#include "Account.hpp"
+#include <cstddef>
+#include <map>
+#include <utility>
+
+size_t Bank::acc_id = 0;
 
 Bank::Bank() : m_liquidity(0) {}
 
 Bank::Bank(int liquidity) : m_liquidity(liquidity) {}
 
+Bank &Bank::operator=(const Bank &other) {
+  if (this == &other)
+    return (*this);
+
+  for (std::map<int, Bank::Account *>::const_iterator it = m_acc_map.begin();
+       it != m_acc_map.end(); it++) {
+    delete it->second;
+  }
+  this->m_acc_map.clear();
+
+  this->m_liquidity = other.m_liquidity;
+  for (std::map<int, Bank::Account *>::const_iterator it =
+           other.m_acc_map.begin();
+       it != other.m_acc_map.end(); it++) {
+    this->m_acc_map.insert(
+        std::pair<int, Account *>(it->first, new Account(*(it->second))));
+  }
+  return (*this);
+}
+
+Bank::Bank(const Bank &other) { *this = other; }
+
+Bank::~Bank() {
+  for (std::map<int, Bank::Account *>::const_iterator it = m_acc_map.begin();
+       it != m_acc_map.end(); it++) {
+    delete it->second;
+  }
+  this->m_acc_map.clear();
+}
+
 std::ostream &operator<<(std::ostream &p_os, const Bank &p_bank) {
   p_os << "Bank informations : " << std::endl;
   p_os << "Liquidity : " << p_bank.getLiquidity() << std::endl;
 
-  for (std::vector<Bank::Account *>::const_iterator it =
-           p_bank.m_clientAccounts.begin();
-       it != p_bank.m_clientAccounts.end(); ++it) {
-    Bank::Account *const &clientAccount = *it;
-    p_os << *clientAccount << std::endl;
+  for (std::map<int, Bank::Account *>::const_iterator it =
+           p_bank.m_acc_map.begin();
+       it != p_bank.m_acc_map.end(); ++it) {
+    p_os << *(it->second) << std::endl;
   }
   return (p_os);
 }
--- m00_encapsulation/ex/Bank.hpp
+++ m00_encapsulation/ex/Bank.hpp
@@ -1,19 +1,27 @@
 #pragma once
 
+#include <cstddef>
 #include <iostream>
+#include <map>
 #include <vector>
 
 class Bank {
 private:
   int m_liquidity;
   class Account;
-  std::vector<Account *> m_clientAccounts;
+  std::map<int, Account *> m_acc_map;
+  static size_t acc_id;
 
 public:
   Bank();
   Bank(int liquidity);
+  Bank(const Bank &);
+  Bank &operator=(const Bank &);
+  ~Bank();
 
   const int &getLiquidity() const;
+  void setLiquidity(int);
+  void updateLiquidity(int);
 
   friend std::ostream &operator<<(std::ostream &p_os,
                                   const Bank::Account &p_account);
